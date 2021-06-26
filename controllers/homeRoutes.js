7866413c401df873cd37359ed1856b6cace36fd5
ForestW70 <hexaforest@gmail.com>
2021-06-26 11:55:30 -0400

    added first home route. started creating home page template with log-in/create user form

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,22 +4,18 @@ const withAuth = require('../utils/auth');
 
 
 // log-in, sign up routes
-router.get('/', withAuth, async (req, res) => {
-  try {
-    // Get all users, sorted by name
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
-
-    // Serialize user data so templates can read it
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data into Handlebars.js template
-    res.render('homepage', { users });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get('/', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/user-dash');
+        return;
+    }
+    
+    res.redirect('/welcome');
+    
 });
+
+router.get('/welcome', withAuth, (req, res) => {
+    res.render('login');
+})
 
 module.exports = router;
