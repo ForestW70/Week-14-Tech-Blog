b9cf2f759dc29dfaefe321ffb2d25e5680c76484
ForestW70 <hexaforest@gmail.com>
2021-06-26 12:57:34 -0400

    transitioned main page and login page to handlebars. added login, create user, and logout functions.

---
+++ public/js/login.js
@@ -0,0 +1,66 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    // Gather the data from the form elements on the page
+    const username = document.getElementById('login-user').value.trim();
+    const password = document.getElementById('login-pass').value.trim();
+
+    if (!username || !password) {
+        alert('Please type in both your username and password.');
+    }
+    
+    try {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/user-dash');
+        } else {
+            alert('Failed to log in');
+        }
+    } catch (err) {
+        console.log(err);
+        alert('something went wrong!');
+    }
+};
+
+const createUserFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('create-user').value.trim();
+    const password = document.getElementById('create-pass').value.trim();
+    const passConfirm = document.getElementById('password-confirm').value.trim();
+
+    if (!password === passConfirm) {
+        alert("Passwords do not match. Please try again.");
+    }
+
+    try {
+        const res = await fetch('/api/users/' + username);
+        if (res.status === 404) {
+            const newUserRes = await fetch('/api/users/add', {
+                method: 'POST',
+                body: JSON.stringify({ username, password }),
+                headers: { 'Content-Type': 'application/json' },
+            })
+
+            if (newUserRes.ok) {
+                document.location.replace('/user-dash');
+            } else {
+                alert('Failed to sign up! Make sure password has 8 or more characters!');
+            }
+        } else {
+            alert('Username already exists. Please try again.');
+        }
+    } catch (err) {
+        console.log(err);
+        alert('something went wrong!');
+    }
+}
+
+
+document.getElementById('loginBtn').addEventListener('click', loginFormHandler);
+document.getElementById('createUserBtn').addEventListener('click', createUserFormHandler);
