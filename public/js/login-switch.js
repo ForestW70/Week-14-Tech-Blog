66a1b9b8787baaa2365e423151e7891ddb47ba25
ForestW70 <hexaforest@gmail.com>
2021-07-03 10:23:18 -0400

    login page overhaul. upgraded styles for login panel. added functionality to switch between panels. added style switch based on which panel youre on. hid password input. added various other styles.

---
+++ public/js/login-switch.js
@@ -0,0 +1,24 @@
+const logInDisplay = document.getElementById('login-screen');
+const createActDisplay = document.getElementById('create-act-screen');
+
+const logPanel = document.getElementById('loginPanel');
+const createPanel = document.getElementById('createAccountPanel');
+
+const displayLogin = () => {
+    logPanel.classList.remove('hide');
+    createPanel.classList.add('hide');
+
+    createActDisplay.classList.remove('on');
+    logInDisplay.classList.add('on');
+}
+
+const displayCreateAct = () => {
+    logPanel.classList.add('hide');
+    createPanel.classList.remove('hide');
+
+    logInDisplay.classList.remove('on');
+    createActDisplay.classList.add('on');
+}
+
+logInDisplay.addEventListener('click', displayLogin);
+createActDisplay.addEventListener('click', displayCreateAct);
